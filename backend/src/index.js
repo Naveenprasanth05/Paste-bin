f6492659df5f9f3c9855462d1b795de46c526bcf
Naveenprasanth05 <npnaveen0001@gmail.com>
2025-12-31 12:45:58 +0530

    Update backend index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ import PasteRouter from "./routes/pasteRoutes.js"
 
 const app = express();
 app.use(cors({
-    origin:"https://paste-bin-frontend.onrender.com",
+    origin:"https://frontend-paste-bin.onrender.com",
     methods:['POST','GET','PUT','DELETE'],
     allowedHeaders:['Content-type','accept','Authorization']
 }));
