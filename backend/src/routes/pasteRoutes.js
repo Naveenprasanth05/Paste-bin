d395ce6dfe0539a2abdbb418ca973629c9e065cf
Naveenprasanth05 <npnaveen0001@gmail.com>
2025-12-31 15:49:35 +0530

    backend folder deleted

--- backend/src/routes/pasteRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { CreatePaste, ViewPaste} from "../controllers/pasteController.js"
-
-const PasteRouter = express.Router();
-PasteRouter.post('/pastes', CreatePaste);
-PasteRouter.get("/pastes/:id",ViewPaste);
-
-export default PasteRouter;
--- backend/src/utils/time.js
+++
@@ -1,4 +0,0 @@
-export function now(req) {
-  const header = req.headers["x-test-now-ms"];
-  return header ? new Date(Number(header)) : new Date();
-}
